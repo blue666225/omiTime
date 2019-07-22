3670a6df8403f9da83ba729764956b118e609f67
blue666225 <fwantazi1145@gmail.com>
2019-07-22 22:35:31 +0900

    ステージ３のプレイヤーのタグの修正、ボスの挙動修正

--- teamOPPAL/Assets/Script/CastleWall.cs
+++ teamOPPAL/Assets/Script/CastleWall.cs
@@ -12,7 +12,7 @@ public class CastleWall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CCount = 1;
     }
 
     // Update is called once per frame
